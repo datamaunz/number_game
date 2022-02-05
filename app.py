c8a26010cb03d3c6b834be8de059bd3b4976d413
datamaunz <datamaunz@gmail.com>
2022-02-05 19:07:07 +0100

    improved version

--- app.py
+++ app.py
@@ -1,40 +1,77 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.graph_objects as go
+from src.helper_functions import *
+
+def main():
+    st.set_page_config(
+    layout="wide",
+    page_title="Number Game",
+    page_icon = "🤖")
+
+    last_lowest_num, highest_num = st.sidebar.slider("lowest card on table", 0,100,[0,100])
+    number_of_remaining_cards = st.sidebar.number_input("How many cards are still in the game?", 1)
 
 
-last_lowest_num, highest_num = st.slider("lowest card on table", 0,100,[0,100])
-number_of_remaining_cards = st.number_input("How many cards are still in the game?", 1)
+    remaining_range = np.arange(last_lowest_num + 1, highest_num + 1)
+    all_probs = [get_prob_that_your_card_is_lowest(x, last_lowest_num, highest_num, number_of_remaining_cards) for x in remaining_range]
+    #create_prob_fig_per_number(remaining_range, all_probs)
 
+    df_new = pd.DataFrame([all_probs]*5).T
+    df_new["number"] = np.arange(last_lowest_num+1, highest_num+1,1)
+    df_new = df_new.set_index("number")
 
-remaining_range = np.arange(last_lowest_num + 1, highest_num + 1)
+    lowest_prob_thresh = 0.5
+    highest_prob_thresh = 1
+    df_first = create_table_for_step(df_new, lowest_prob_thresh, highest_prob_thresh)
 
+    lowest_prob_thresh = 0.5
+    highest_prob_thresh = 0.75
+    df_second = create_table_for_step(df_new, lowest_prob_thresh, highest_prob_thresh)
 
+    lowest_prob_thresh = 0.75
+    highest_prob_thresh = 0.875
+    df_third = create_table_for_step(df_new, lowest_prob_thresh, highest_prob_thresh)
 
-def get_prob_that_your_card_is_lowest(your_card, last_lowest_num, highest_num, number_of_remaining_cards):
+    st.markdown("## Emotional Robots")
+
+    x = df_first.index[-2]
+
+    f"""
+    ### Steps
     
-    interval = highest_num - last_lowest_num
-    prob = (your_card - last_lowest_num - 1)  / interval
-    card_higher = 1 - prob
-    all_cards_higher = card_higher ** (number_of_remaining_cards)
-    return all_cards_higher
+    1. Set the slider to the lowest number that has been put on the table. If no card is on the table yet, set it to 0.
+    2. You're the only person smiling? **You have the lowest card**! Put it on the table and start again at step 1.
+    3. First round (*table 1*) and everyone is yawning? - Move the slider up to `{x}` and go back to step 2.
+    4. More then 1 person is excited? The lowest card is among you (the excited folks). You are about to enter round 2 (*table 1*).
+        
+        5.1. You're the only person smiling? You are holding the lowest card. Put it on the *table* and go back to step 1.
+        
+        5.2. More than one person smiling? Same as before: move on to the next round (3, then 4 etc.) in *table* 1 (step 4).
+        
+        5.3. Everyone yawning at round 2? Go to *table* 2!
+        
+        5.3. Everyone yawning at round 3? Go to *table* 3!
+    """
 
-all_probs = [get_prob_that_your_card_is_lowest(x, last_lowest_num, highest_num, number_of_remaining_cards) for x in remaining_range]
+    col1, col2, col3 = st.columns(3)
+        
 
-fig = go.Figure()
-fig.add_traces(go.Scatter(
-    x = remaining_range,
-    y = all_probs
-    ))
-
-fig.update_layout(
-    plot_bgcolor='rgba(0,0,0,0)',
-    paper_bgcolor='rgba(0,0,0,0)',
-    xaxis=dict(gridcolor="lightgrey", title=dict(text="card number")),
-    yaxis=dict(gridcolor="lightgrey", title=dict(text="probability that card is the lowest"))
-)
-st.plotly_chart(fig, use_container_width=True)
+    with col1:
+        if df_first.empty == False:
+            st.markdown("### Table 1")    
+            st.write(df_first)
 
 
+    with col2:
+        if df_second.empty == False:
+            st.markdown("### Table 2")
+            st.write(df_second)
 
+    with col3:
+        if df_third.empty == False:
+            st.markdown("### Table 3")
+            st.write(df_third)
+
+
+if __name__ == '__main__':
+    main()
+    
---
+++ src/helper_functions.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+
+def get_prob_that_your_card_is_lowest(your_card, last_lowest_num, highest_num, number_of_remaining_cards):
+    
+    interval = highest_num - last_lowest_num
+    prob = (your_card - last_lowest_num - 1)  / interval
+    card_higher = 1 - prob
+    all_cards_higher = card_higher ** (number_of_remaining_cards)
+    return all_cards_higher
+
+def replace_by_emotion(number, lowest_prob_thresh, highest_prob_thresh):
+    if (number >= lowest_prob_thresh) & (number < highest_prob_thresh):
+        return "😃"
+    else:
+        return "🥱"
+
+
+def replace_by_binary(number, lowest_prob_thresh, highest_prob_thresh=1):
+    if (number >= lowest_prob_thresh) & (number <= highest_prob_thresh):
+        return 1
+    else:
+        return 0
+
+def get_last_relevant_row(df):
+    if len(df.columns) > 1:
+        last_remaining_row = df[df.duplicated() == False].index[-1]
+        df = df.loc[:last_remaining_row]
+        return df
+    else:
+        
+        relevant_index = df[df == 0].index[2]
+        df = df.loc[:relevant_index]
+        return df
+    
+def calculate_next_lowest_prob_thresh(lowest_prob_thresh, highest_prob_thresh):
+    return lowest_prob_thresh + (highest_prob_thresh - lowest_prob_thresh) / 2
+
+def calculate_next_highest_prob_thresh(lowest_prob_thresh, highest_prob_thresh):
+    return highest_prob_thresh -  ((highest_prob_thresh - lowest_prob_thresh) / 2)
+
+def create_table_for_step(df_new, lowest_prob_thresh, highest_prob_thresh):
+    
+    df_table = df_new.copy()
+    df_table = df_table[df_table <= highest_prob_thresh].dropna()
+    for column in df_new.columns:
+        df_table[column] = df_table[column].apply(lambda x: replace_by_binary(x, lowest_prob_thresh))
+        lowest_prob_thresh = calculate_next_lowest_prob_thresh(lowest_prob_thresh, highest_prob_thresh)
+        if df_table[column].sum() < 2:
+            df_table = df_table.drop(column, 1)
+            
+    df_table = get_last_relevant_row(df_table)
+    df_table.columns = [f'round {x+1}' for x in df_table.columns]
+    
+    if len(df_table) > 0:
+        df_table = df_table.applymap(lambda x: {0:"🥱", 1:"😃"}.get(x))
+    else:
+        df_table = None
+    
+    return df_table
+
+def create_prob_fig_per_number(remaining_range, all_probs):
+    
+    fig = go.Figure()
+    fig.add_traces(go.Scatter(
+        x = remaining_range,
+        y = all_probs
+        ))
+
+    fig.update_layout(
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)',
+        xaxis=dict(gridcolor="lightgrey", title=dict(text="card number")),
+        yaxis=dict(gridcolor="lightgrey", title=dict(text="probability that card is the lowest"))
+    )
+    st.plotly_chart(fig, use_container_width=True)
